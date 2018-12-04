3aac3bcfd3c731e5be72a7ffbbd03f1bd26eab88
Geeth sandaru Madhushan <geeth.gsm@gmail.com>
2018-12-04 13:31:57 +0530

    new field to model and db

--- WebApplication1/Pages/Movies/Index.cshtml.cs
+++ WebApplication1/Pages/Movies/Index.cshtml.cs
@@ -24,8 +24,10 @@ namespace WebApplication1.Pages.Movies
         public SelectList Genres { get; set; }
         public string MovieGenre { get; set; }
 
-        public async Task OnGetAsync(string searchString)
+        public async Task OnGetAsync(string movieGenre, string searchString)
         {
+            IQueryable<string> genreQuery = from m in _context.Movie orderby m.Genre select m.Genre;
+
             var movies = from m in _context.Movie select m;
             
             if (!String.IsNullOrEmpty(searchString))
@@ -33,6 +35,12 @@ namespace WebApplication1.Pages.Movies
                 movies = movies.Where(s => s.Title.Contains(searchString));
             }
 
+            if (!String.IsNullOrEmpty(movieGenre))
+            {
+                movies = movies.Where(x => x.Genre.Equals(movieGenre));
+            }
+
+            Genres = new SelectList(await genreQuery.Distinct().ToListAsync());
             Movie = await movies.ToListAsync();
             SearchString = searchString;
         }
